4aa982b4cf96516b86fd7bc60b9b51f03d8b42fe
vitallik <tkashuta@gmail.com>
2023-11-30 13:39:31 +0200

    feat: add docker secrets support

---
+++ scripts/import_docker_secrets.sh
@@ -0,0 +1,22 @@
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		mysql_error "Both $var and $fileVar are set (but are exclusive)"
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
+docker_setup_env() {
+	file_env 'LAGO_RSA_PRIVATE_KEY'
+}
+
+docker_setup_env
